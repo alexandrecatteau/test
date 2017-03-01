93aa9c1c02e4d5e9a16589147c2e3df879fac6f3
Alex <Alex@DESKTOP-AITTL8D>
2017-03-01 21:10:09 +0100

    maj

--- WindowsFormsApplication1/test.cs
+++ WindowsFormsApplication1/test.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
     class test
     {
+        public test()
+        {
+            MessageBox.Show("test");
+        }
     }
 }
